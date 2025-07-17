65295972414d17c22d331783ad166b61536f6cd1
s7zs <hussinayman766@gmail.com>
2025-07-17 11:32:50 +0300

    fixied

--- src/main/java/com/task/task1/controller/Homecontroller.java
+++ src/main/java/com/task/task1/controller/Homecontroller.java
@@ -4,9 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.task.task1.services.Messageservice;
-
 import com.task.task1.Messageservice;
+
 
 @RestController
 public class HomeController {
